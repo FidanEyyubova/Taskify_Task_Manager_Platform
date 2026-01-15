781cc5567dae9fae8cb6cb161294072bff0c4887
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2026-01-15 14:19:55 +0400

    Setup Clerk middleware and Start Navbar

---
+++ README.md
@@ -0,0 +1,2 @@
+# 🚧 Work in Progress 🚧
+This project is currently under development. Expect changes and unfinished features.
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar"
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-[#e9f9c5] via-white">
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
